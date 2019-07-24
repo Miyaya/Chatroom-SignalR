38b77c27a375fee29fab0b45eca655d52995eda9
Miyaya <miya850604@gmail.com>
2019-07-24 10:50:21 +0800

    Add files via upload

---
+++ Practice03/wwwroot/js/chat.js
@@ -0,0 +1,36 @@
+﻿"use strict";
+
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+//Disable send button until connection is established
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var now = new Date();
+    var HH = now.getHours();
+    var mm = now.getMinutes();
+    var ss = now.getSeconds();
+    
+    var msg = message.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;");
+    var encodedMsg = "[" + HH + ":" + mm + ":" + ss + "] " +
+        user + ": " + msg;
+    //var encodedMsg = user + ": " + msg;
+    var div = document.createElement("div");
+    div.textContent = encodedMsg;
+    document.getElementById("messagesList").appendChild(div);
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+}).catch(function (err) {
+    return console.error(err.toString());
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message).catch(function (err) {
+        return console.error(err.toString());
+    });
+    event.preventDefault();
+});
